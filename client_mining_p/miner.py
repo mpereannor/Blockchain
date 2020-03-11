2989d3b2bbd3ad516bb3544e3063af2f682cb27d
chudnofskyATD <mpereannor@gmail.com>
2020-03-11 22:55:30 +0000

    mining implemented

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,8 +4,8 @@ import requests
 import sys
 import json
 
-
-# def proof_of_work(block):
+DIFFICULTY = 3 
+def proof_of_work(block):
 #     """
 #     Simple Proof of Work Algorithm
 #     Stringify the block and look for a proof.
@@ -14,6 +14,11 @@ import json
 #     :return: A valid proof for the provided block
 #     """
 #     pass
+  block_string = json.dumps(last_block, sort_keys=True)
+  proof = 0
+  while valid_proof(block_string, proof) is False:
+    proof += 1 
+  return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,11 +32,13 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
-
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    
+    return guess_hash[:DIFFICULTY] == '0' * DIFFICULTY
 
 if __name__ == '__main__':
-    # What is the server address? IE `python3 miner.py https://server.com/api/`
+  # What is the server address? IE `python3 miner.py https://server.com/api/`
     if len(sys.argv) > 1:
         node = sys.argv[1]
     else:
@@ -42,6 +49,8 @@ if __name__ == '__main__':
     id = f.read()
     print("ID is", id)
     f.close()
+    
+    coins_mined = 0
 
     # Run forever until interrupted
     while True:
@@ -55,16 +64,22 @@ if __name__ == '__main__':
             print(r)
             break
 
-        # TODO: Get the block from `data` and use it to look for a new proof
+        last_block = data['last_block']
         # new_proof = ???
+        new_proof = proof_of_work(last_block)
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
-        data = r.json()
-
-        # TODO: If the server responds with a 'message' 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
-        # print the message from the server.
-        pass
+        try:
+          data = r.json()
+        except:
+          print("Error:  Non-json response")
+          print("Response returned:")
+          print(r)
+          break
+        
+        if data['message'] == 'New Block Forged':
+          coins_mined += 1
+        print(f'coins{coins_mined}')
